cc503f65e889f8aaebe44a846de3094268c498d0
Sadiq Okocha <sokocha@gmail.com>
2018-01-21 14:00:24 +0000

    JSON working in the console

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,41 @@
 import express from 'express';
 
+
 const router = express.Router();
+var request = require('request');
 
 /* GET index page. */
 router.get('/', (req, res) => {
   res.render('index', {
-    title: 'Express'
+    title: 'Bitcoin For Beginners'
   });
 });
 
+// router.get('/currency', (req, res, next) => {
+//   var coins = request({
+//     uri: 'https://api.coinmarketcap.com/v1/ticker/?limit=10'
+//   });
+//   res.send('currency', {coins});
+// });
+
+router.get('/currency', (req, res, next) => {
+  request("https://api.coinmarketcap.com/v1/ticker/?limit=10", function(err,res,body) {
+    var coinList = JSON.parse(body);
+    console.log(coinList);
+
+  });
+  res.render('currency',{coinList});
+});
+
+
+
+router.get('/test', (req, res, next) => {
+  var coins = request({
+    uri: 'https://api.coinmarketcap.com/v1/ticker/?limit=10'
+  }).pipe(res);
+  console.log(coins)
+});
+
 export default router;
+
+// .pipe(res)
